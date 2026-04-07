0a6f3f44b43c019f1d624f9aaf93bcce66c1c82e
ziaulkadri <ziaulkadri1600@gmail.com>
2026-04-07 23:51:27 +0530

    added mongodb

---
+++ src/db/index.js
@@ -0,0 +1,18 @@
+import mongoose, { connect } from "mongoose";
+
+
+
+const connectDb = async () => {
+  try {
+    await connect(process.env.Mongo_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    process.exit(1); // Exit the process with an error code
+  }
+};
+
+export default connectDb;
