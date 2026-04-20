4f5f5a126c9a8464985c29093d1f89802fa5a541
ziaulkadri <ziaulkadri1600@gmail.com>
2026-04-20 23:46:43 +0530

    added validation middleware

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,10 +2,15 @@ import { Router } from "express";
 
 import { login, register } from "../controllers/auth.controllers.js";
 import { asyncHandler } from "../utils/async-handler.js";
+import { validate } from "../midllewares/validator.middleware.js";
+import { userLoginValidator, userRegisterValidator } from "../validators/index.js";
 
 const router = Router()
 
-router.post("/register",asyncHandler(register))
-router.post("/login",asyncHandler(login))
+// router.post("/register", validate(userRegisterValidator()), asyncHandler(register))
+// router.post("/login", validate(userLoginValidator()), asyncHandler(login))
+
+router.route("/register").post(userRegisterValidator(), validate, asyncHandler(register))
+router.route("/login").post(userLoginValidator(), validate, asyncHandler(login))
 
 export default router
